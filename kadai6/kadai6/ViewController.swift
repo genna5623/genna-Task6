80a798ca0919e7a8cde04e0e05a7304204b99500
akio0911 <akio0911@gmail.com>
2021-05-03 10:35:16 +0900

    feedback

--- kadai6/kadai6/ViewController.swift
+++ kadai6/kadai6/ViewController.swift
@@ -10,37 +10,45 @@ class ViewController: UIViewController {
     
     @IBOutlet private var questionLabel: UILabel!
     
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet private weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        reStart()
+        reset()
     }
-    var questionNum:Int = 0
-    func reStart(){
+
+    private var questionNum: Int = 0
+
+    private func reset(){
         questionNum = Int.random(in: 1...100)
         questionLabel.text = String(questionNum)
         slider.value = 50
     }
-    func resultAlert (message:String) {
+
+    private func resultAlert (message: String) {
         let firstTitle = "結果"
         let alert = UIAlertController(title: firstTitle, message: message, preferredStyle: .alert)
         
-        let alertControler = UIAlertAction(title: "再挑戦", style: .default, handler: { [weak self] action in self?.reStart() })
+        let alertControler = UIAlertAction(
+            title: "再挑戦",
+            style: .default,
+            handler: { [weak self] action in
+                self?.reset()
+            }
+        )
         alert.addAction(alertControler)
+
         present(alert, animated: true, completion: nil)
     }
+
     @IBAction func resultButton(_ sender: Any) {
         let sliderNum = Int(slider.value)
-        let secondRow:String
+        let secondRow: String
         if sliderNum == questionNum {
             secondRow = "あたり！"
         } else {
             secondRow = "はずれ！"
         }
-        resultAlert( message: "\(secondRow)  \nあなたの値: \(sliderNum)")
-        
+        resultAlert(message: "\(secondRow)\nあなたの値: \(sliderNum)")
     }
-    
 }
-
